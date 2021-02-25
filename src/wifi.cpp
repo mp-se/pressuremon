ae6592e8896994c09cfab74e0b955173e7e59074
Magnus <magnus@noreply.github.com>
2021-02-25 16:01:19 +0100

    Added id checks for reset/wifi

--- src/wifi.cpp
+++ src/wifi.cpp
@@ -82,25 +82,35 @@ void webHandleConfig() {
 // Callback from webServer when / has been accessed.
 //
 void webHandleCalibrate() {
+    String id    = myWebServer.arg("id");
 #if LOG_LEVEL==6
     Log.verbose(F("WIFI: webServer callback for /calibrate." CR));
 #endif
-    myPressureSensor.calibrateSensor();
-    char buf[40];
-    sprintf(&buf[0], "Calibrated pressure sensor, new offset %s...", myConfig.getPressureZeroCorrection() );
-    myWebServer.send(200, "text/plain", &buf[0] );
+    if( !id.compareTo( myConfig.getID() ) ) {
+        myPressureSensor.calibrateSensor();
+        char buf[40];
+        sprintf(&buf[0], "Calibrated pressure sensor, new offset %s...", myConfig.getPressureZeroCorrection() );
+        myWebServer.send(200, "text/plain", &buf[0] );
+    } else {
+        myWebServer.send(400, "text/plain", "Unknown ID.");
+    }
 }
 
 //
 // Callback from webServer when / has been accessed.
 //
 void webHandleReset() {
+    String id    = myWebServer.arg("id");
 #if LOG_LEVEL==6
     Log.verbose(F("WIFI: webServer callback for /reset." CR));
 #endif
-    myWebServer.send(200, "text/plain", "Doing reset...");
-    delay(1000);
-    ESP.reset();
+    if( !id.compareTo( myConfig.getID() ) ) {
+        myWebServer.send(200, "text/plain", "Doing reset...");
+        delay(1000);
+        ESP.reset();
+    } else {
+        myWebServer.send(400, "text/plain", "Unknown ID.");
+    }
 }
 
 //
@@ -130,13 +140,18 @@ void webHandleStatus() {
 // Callback from webServer when / has been accessed.
 //
 void webHandleClearWIFI() {
+    String id    = myWebServer.arg("id");
 #if LOG_LEVEL==6
     Log.verbose(F("WIFI: webServer callback for /clearwifi." CR));
 #endif
-    myWebServer.send(200, "text/plain", "Clearing WIFI credentials and doing reset...");
-    delay(1000);
-    WiFi.disconnect();  // Clear credentials
-    ESP.reset();
+    if( !id.compareTo( myConfig.getID() ) ) {
+        myWebServer.send(200, "text/plain", "Clearing WIFI credentials and doing reset...");
+        delay(1000);
+        WiFi.disconnect();  // Clear credentials
+        ESP.reset();
+    } else {
+        myWebServer.send(400, "text/plain", "Unknown ID.");
+    }
 }
 
 //
