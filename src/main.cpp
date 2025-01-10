e88ef0ee8aeda54f8fa5b7d457a5687466ae18a3
Magnus <mp-se@noreply.github.com>
2025-01-10 08:39:35 +0100

    Adding i2c scanner

--- src/main.cpp
+++ src/main.cpp
@@ -25,17 +25,37 @@ SOFTWARE.
 #include <pressure.hpp>
 #include <config.hpp>
 #include <looptimer.hpp>
+#include <Wire.h>
+#include <pins_arduino.h>
 
 SerialDebug mySerial(115200L);
 PressConfig myConfig;
 PressureSensor myPressureSensor;
 
-void setup()
-{
+void scanI2C() {
+  for (int i = 1, j = 0; i < 127; i++) {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(i);
+    int err = Wire.endTransmission();
+
+    if (err == 0) {
+      Log.notice(F("WEB : Found device at 0x%02X." CR), i);
+      j++;
+    }
+  }
+}
+
+void setup() {
+  delay(2000); // Allow for usbc serial to connect
+
   Log.notice(F("Main: Starting up." CR));
+  Log.notice(F("Main: OneWire SDA=%d, SCL=%d." CR), SDA, SCL);
+
+  scanI2C();
 
   // This is just for testing and evaluating the sensors
-
   // Code base will be merged into the gravitymon project to enable sharing of the common code and make maintenance easier
 
   myConfig.setPressureSensorType(PressureSensorType::SensorCFSensorXGZP6847DGaugeKPa_700);
@@ -45,11 +65,8 @@ void setup()
 
 LoopTimer timer(2000);
 
-void loop()
-{
-
-  if (timer.hasExipred())
-  {
+void loop() {
+  if (timer.hasExipred()) {
     timer.reset();
     myPressureSensor.loop();
 
